2ab792ace0516e6456940988573a43fa4016df4e
Kobe <kobeneilson@gmail.com>
2023-10-10 22:25:04 -0700

    Worked on the logic more, the javascript ideas are coming together

--- Projects/Scripts/caluclator.js
+++ Projects/Scripts/caluclator.js
@@ -2,46 +2,82 @@ const outputDisplay = document.querySelector('.js-output');
 const numberButton = document.querySelector('.js-button-number');
 const divButtonGrid = document.querySelector('.js-button-grid');
 const clearButton = document.querySelector('.js-clear-button');
-const calculateCheck = toggleTerm ? tempTerm = baseTerm : calculateValues(baseTerm, augmentTerm);
 
 let operation = '';
 let toggleTerm = true;
 let tempTerm = '';
+let baseTerm = '';
+let augmentTerm = '';
+const calculateCheck = toggleTerm ? tempTerm = baseTerm : calculateValues(Number(baseTerm), Number(augmentTerm));
 
 divButtonGrid.addEventListener('click', (event) => {
   const clickedButton = event.target;
-
   if (!clickedButton.classList.contains('js-button-number')) {
     return;
   };
+
   tempTerm += clickedButton.innerHTML;
   outputDisplay.innerHTML = tempTerm;
 });
 
-clearButton.addEventListener('click', () => outputDisplay.innerHTML = '');
+divButtonGrid.addEventListener('click', (event) => {
+  const button = event.target
+  if (!button.classList.contains('js-button-operation')) {
+    return;
+  };
 
-function addition() {
   toggleTerm = false;
-  operation = 'add';
-  calculateCheck;  
+  baseTerm = tempTerm;
+  tempTerm = '';
+  outputDisplay.innerHTML = tempTerm;
+  calculateCheck;
+  console.log(baseTerm);
+
+
+  if (button.innerHTML === '+') {
+    operation = 'add';
+  }  else if (button.innerHTML === '-') {
+    operation = 'subtract';
+  } else if (button.innerHTML === '*') {
+    operation = 'multiply';
+  } else if (button.innerHTML === '/') {
+    operation = 'devide';
+  }
+});
+
+divButtonGrid.addEventListener('click', (event) => {
+  const button = event.target
+  if (!button.classList.contains('js-button-equals')) {
+    return;
+  }
+  if (!baseTerm === '' && !augmentTerm === '') {
+    calculateValues(Number(baseTerm), Number(augmentTerm))
+    console.log('equals check')
+  }
+})
+
+clearButton.addEventListener('click', () => {
+  outputDisplay.innerHTML = '';
+  tempTerm = '';
+});
+
+function equal() {
+  if (toggleTerm) {
+    return;
+  }
+  tempTerm = augmentTerm
+  calculateValues(Number(baseTerm), Number(augmentTerm))
 }
 
-function subtract() {
-  toggleTerm = false;
-  operation = 'subtract';
-  calculateCheck;  
+function calculateValues(base, augment) {
+  if (operation === 'add') {
+    base += augment;
+  } else if (operation === 'subtract') {
+    base -= augment;
+  } else if (operation === 'multiply') {
+    base = base * augment;
+  } else if (operation === 'devide') {
+    base = base / augment;
+  };
+  console.log(base);
 }
-
-function multiplication() {
-  toggleTerm = false;
-  operation = 'multiply';
-  calculateCheck;  
-}
-
-function devide() {
-  toggleTerm = false;
-  operation = 'devide';
-  calculateCheck;  
-}
-
-function calculateValues()
